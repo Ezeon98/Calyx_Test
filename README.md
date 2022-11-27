5cf4218b2ea685c54fedfdc304f531bfe987dfe5
Ezeon98 <ezeon3@gmail.com>
2022-11-27 00:11:21 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Calyx_Test
+
+Steps to execute:
+
+In console:
+
+- virtualenv -p python3 calyx
+- .\calyx\Scripts\activate
+- pip install -r requirements.txt
+
+Then execute "main.py"
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.postgre_connect import create_connection
 from src.create_tables_scripts import create_tables
 from src.inject_data import inject_data
 from src.input_files import main
+from src.logger import log
 
 files=['main','cinema','province','category']
 
@@ -9,3 +10,4 @@ main()
 # create_tables(create_connection())
 for i in files:
     inject_data(create_connection(), i)
+log('inject succeful', 'info')
