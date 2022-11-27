6aa55ef7f1c7183cf14fea579901d7ca7a6e6017
Ezeon98 <ezeon3@gmail.com>
2022-11-27 00:30:48 -0300

    Rework input_files.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from src.postgre_connect import create_connection
 from src.create_tables_scripts import create_tables
 from src.inject_data import inject_data
-from src.input_files import main
+from src.input_files import get_files
 
 files=['main','cinema','province','category']
 
-main()
+get_files()
 # create_tables(create_connection())
 for i in files:
     inject_data(create_connection(), i)
