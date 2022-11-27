eedbe3e9282608a5f4c22006bdee315502334d36
Ezeon98 <ezeon3@gmail.com>
2022-11-27 20:27:12 -0300

    Add more comments in preprocessing data file

--- src/preprocessing_data.py
+++ src/preprocessing_data.py
@@ -22,17 +22,25 @@ def build_main_dataSet(dataframes):
     dataframes = list: The three datasets (cinema, museums,libraries)
 
     '''
-    pd_data = pd.concat(dataframes)
+    pd_data = pd.concat(dataframes) 
     pd_data['provincia'] = pd_data['provincia'].apply(unidecode) #Quit the accents
     pd_data['fecha'] = now # Add the 'fecha' column with actual Datetime
     return pd_data
 
 def fix_data(path):
     '''
+    Normalize the datasets columns and add missing columns
+
+    Returns a normalized DataSet
+
+    Params:
+    path = String : Path of the csv file to process
     '''
-    df = pd.read_csv(path)
-    df.columns = df.columns.str.lower()
-    df.columns = [unidecode(col) for col in df.columns]
+    df = pd.read_csv(path) 
+    df.columns = df.columns.str.lower() # Quit capital letters
+    df.columns = [unidecode(col) for col in df.columns] #Quit Accents
+
+    # Normalize columns names
     if 'domicilio' in df.columns:
         df.rename(columns = {'domicilio':'direccion'}, inplace = True)
     if 'cod_localidad' in df.columns:
@@ -41,14 +49,26 @@ def fix_data(path):
         df.rename(columns = {'id_provincia':'idprovincia'}, inplace = True)
     if 'id_departamento' in df.columns:
         df.rename(columns = {'id_departamento':'iddepartamento'}, inplace = True)
+    
+    # Add missing columns
     for c in COLUMNS:
         if c not in df.columns:
             df[c] = None
+    
+    # Select the correct columns
     df = df.filter(items=COLUMNS)
     return df
 
 def category_file(pd_data):
+    '''
+    Build the category dataframe to input in database
+    Returns a DataFrame
 
+    Params:
+
+    pd_data = DataFrame : The main dataframe to extract the category column
+    '''
+    #Build the DataFrame
     category_df = pd.DataFrame({"Categoria":['Salas de cine',
                                      'Bibliotecas Populares',
                                      'Espacios de Exhibición Patrimonial'],
@@ -56,24 +76,39 @@ def category_file(pd_data):
                                     len(pd_data[pd_data['categoria']=='Bibliotecas Populares']),
                                     len(pd_data[pd_data['categoria']=='Espacios de Exhibición Patrimonial'])]})
     
-    category_df['fecha'] = now
+    category_df['fecha'] = now # Add the 'fecha' column with actual Datetime
+
     return category_df
 
 def province_file(pd_data):
-    province_df = pd_data[['provincia']]
-    province_df = province_df.groupby(["provincia"])["provincia"].count()
-    province_df = province_df.reset_index(name='cantidad')
-    province_df['fecha'] = now
+    '''
+    Build the province dataframe to input in database
+    Returns a DataFrame
+
+    Params:
+
+    pd_data = DataFrame : The main dataframe to group by "provincia" column
+    '''
+    province_df = pd_data[['provincia']] #Select the "provincia" column
+    province_df = province_df.groupby(["provincia"])["provincia"].count() #Count by province
+    province_df = province_df.reset_index(name='cantidad') #Add "Cantidad" column
+    province_df['fecha'] = now # Add the 'fecha' column with actual Datetime
     return province_df
 
 def cinema_file():
-    file_path = f"files/cines/{today.year}-{monthName}/cines-{today}.csv"
-    cinema_df = pd.read_csv(file_path)
+    '''
+    Build the cinema dataframe to input in database
+    Returns a DataFrame
+    '''
+    file_path = f"files/cines/{today.year}-{monthName}/cines-{today}.csv" #Locate the cine file path
+    cinema_df = pd.read_csv(file_path) #Build the dataframe
     
-    cinema_df = cinema_df.filter(items=['provincia','pantallas', 'butacas','espacio_incaa'])
-    cinema_df['espacio_incaa'] = cinema_df['espacio_incaa'].replace(['Si','No'], [1,0])
-    cinema_df = cinema_df.groupby(["provincia"]).sum()
+    cinema_df = cinema_df.filter(items=['provincia','pantallas', 'butacas','espacio_incaa']) #Select columns
+    cinema_df['espacio_incaa'] = cinema_df['espacio_incaa'].replace(['Si','No'], [1,0]) #Replace bool to numbers
+    cinema_df = cinema_df.groupby(["provincia"]).sum() # Group by "provincia" column to get sums of the other columns
+
     cinema_df=cinema_df.reset_index()
-    cinema_df['fecha'] = now
+    
+    cinema_df['fecha'] = now # Add the 'fecha' column with actual Datetime
     
     return cinema_df
